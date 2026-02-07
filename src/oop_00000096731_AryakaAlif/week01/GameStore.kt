74aa6d342ff9911eb11bd3dac394f41c91d344e1
Aryaka Alif <ardianun212@gmail.com>
2026-02-07 21:28:42 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000096731_AryakaAlif/week01/GameStore.kt
+++ src/oop_00000096731_AryakaAlif/week01/GameStore.kt
@@ -1,2 +1,12 @@
 package oop_00000096731_AryakaAlif.week01
 
+fun main() {
+    val gameTitle = "Mobile Legends"
+    val price = 750000
+    println(calculateDiscount(price))
+
+
+}
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000) price - (price * 20/100)
+    else price - (price * 10/100)
