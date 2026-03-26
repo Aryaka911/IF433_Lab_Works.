248526beae944d5a5562223c97a293c54358a70d
Aryaka Alif <ardianun212@gmail.com>
2026-03-26 23:56:24 +0700

    add warning validation in init block

--- src/oop_000000996731_AryakaAlif_week2/Student.kt
+++ src/oop_000000996731_AryakaAlif_week2/Student.kt
@@ -1,9 +1,13 @@
 package oop_000000996731_AryakaAlif_week2
 
-class Student (
-    val name: String,
-    val nim: String,
-    val major: String,
-){
+class Student(val name: String, val nim: String) {
 
+    init {
+        if (nim.length != 5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+        } else {
+            println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
+        }
+    }
 }
