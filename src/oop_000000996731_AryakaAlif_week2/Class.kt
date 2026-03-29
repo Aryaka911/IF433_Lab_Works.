65d18383fd77711513911f9d81e6df09dd7363be
Aryaka Alif <ardianun212@gmail.com>
2026-03-30 04:54:15 +0700

    week02: implement user input and pre-validation

---
+++ src/oop_000000996731_AryakaAlif_week2/Class.kt
@@ -0,0 +1,14 @@
+package oop_000000996731_AryakaAlif_week2
+
+class MyCar {
+    var warna:String = "Merah"
+
+    fun kecepatan() {
+        println("Mobil saya warna $warna")
+    }
+}
+
+fun main() {
+    val myCar = MyCar()
+    myCar.kecepatan()
+}
