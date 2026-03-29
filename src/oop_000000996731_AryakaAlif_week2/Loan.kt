2e0a41456e07f8a2839c99cead32c5ef16772a65
Aryaka Alif <ardianun212@gmail.com>
2026-03-30 04:59:51 +0700

    week02: (task 1) create Loan class with default args

---
+++ src/oop_000000996731_AryakaAlif_week2/Loan.kt
@@ -0,0 +1,14 @@
+package oop_000000996731_AryakaAlif_week2
+
+
+import java.util.Scanner
+
+class Loan (val bookTitle: String, val borrower: String, val loanDuration: Int = 1) {
+    fun calculateFine(): Int {
+        return if (loanDuration > 3) {
+            (loanDuration - 3) * 2000
+        } else {
+            0
+        }
+    }
+}
