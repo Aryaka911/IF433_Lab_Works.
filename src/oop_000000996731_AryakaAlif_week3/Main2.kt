c70528bddfa003008159f3702599f154d2a5e6c3
Aryaka Alif <ardianun212@gmail.com>
2026-03-30 05:21:30 +0700

    week03: (task 2) implement Player testing in main

--- src/oop_000000996731_AryakaAlif_week3/Main2.kt
+++ src/oop_000000996731_AryakaAlif_week3/Main2.kt
@@ -2,6 +2,12 @@ package oop_000000996731_AryakaAlif_week3
 
 fun main() {
     val weapon = Weapon("Dragon Slayer")
+    val player = Player("Kyoodain")
+
+    player.addXp(50)
+    player.addXp(60)
+
+    println("Level sekarang: ${player.level}")
 
     weapon.damage = -50
     weapon.damage = 9999
