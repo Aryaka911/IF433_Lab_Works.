525650da940291e4c3b4da7768677bed631ce811
Aryaka Alif <ardianun212@gmail.com>
2026-03-29 23:45:53 +0700

    week07: (trial) trigger private constructor error on NetworkClient

---
+++ src/oop_000000996731_AryakaAlif_week7/Main.kt
@@ -0,0 +1,6 @@
+package oop_000000996731_AryakaAlif_week7
+
+fun main() {
+    val client = NetworkClient("https://api.umn.ac.id")
+    client.connect()
+}
---
+++ src/oop_000000996731_AryakaAlif_week7/NetworkClient.kt
@@ -0,0 +1,8 @@
+package oop_000000996731_AryakaAlif_week7
+
+class NetworkClient private constructor(val url: String) {
+    fun connect() {
+        println("Connecting to $url...")
+    }
+} {
+}
