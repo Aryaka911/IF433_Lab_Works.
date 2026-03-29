db25727c3bce3794fe2df0a9d84cdaf79869dda5
Aryaka Alif <ardianun212@gmail.com>
2026-03-30 05:19:39 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_000000996731_AryakaAlif_week3/Main2.kt
+++ src/oop_000000996731_AryakaAlif_week3/Main2.kt
@@ -1,2 +1,11 @@
 package oop_000000996731_AryakaAlif_week3
 
+fun main() {
+    val weapon = Weapon("Dragon Slayer")
+
+    weapon.damage = -50
+    weapon.damage = 9999
+
+    println("Damage akhir: ${weapon.damage}")
+    println("Tier: ${weapon.tier}")
+}
