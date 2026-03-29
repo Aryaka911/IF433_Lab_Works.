006f5854ab205db2b830919b9aa29ebe74428695
Aryaka Alif <ardianun212@gmail.com>
2026-03-30 05:05:49 +0700

    week03: fix setter using backing field

--- src/oop_000000996731_AryakaAlif_week3/Main.kt
+++ src/oop_000000996731_AryakaAlif_week3/Main.kt
@@ -1,4 +1,8 @@
 package oop_000000996731_AryakaAlif_week3
 
 class Main {
-}
+    fun main() {
+        val e = Employee("Budi")
+        println("--- Test Error ---")
+        e.salary = 5000
+    }
