8bb4efea34378b761cec14d251c7ba7a0ec46ede
Aryaka Alif <ardianun212@gmail.com>
2026-03-30 00:26:20 +0700

    week07: (task) test data class copy and sealed class event dispatch

--- src/oop_000000996731_AryakaAlif_week7/Main1.kt
+++ src/oop_000000996731_AryakaAlif_week7/Main1.kt
@@ -4,4 +4,25 @@ fun main() {
     println("=== TEST GAME MANAGER ===")
     GameManager.startGame()
     GameManager.startGame()
+
+    println("\n=== TEST RARITY ENUM ===")
+    println("Legendary drop chance: ${ItemRarity.LEGENDARY.dropChance}%")
+
+    println("\n=== TEST WEAPON FACTORY ===")
+    val starterWeapon = Weapon.forgeStarterSword()
+    println("Senjata awal: ${starterWeapon.item.name}, Damage: ${starterWeapon.item.damage}, Rarity: ${starterWeapon.item.rarity}")
+
+    println("\n=== TEST DATA CLASS COPY ===")
+    val upgradedItem = starterWeapon.item.copy(damage = 25)
+    println("Item setelah upgrade: $upgradedItem")
+
+    println("\n=== TEST SEALED CLASS EVENTS ===")
+
+    processEvent(BattleState.SafeZone)
+
+    processEvent(BattleState.MonsterEncounter("Goblin Nakal"))
+
+    processEvent(BattleState.LootDropped(upgradedItem))
+
+    processEvent(BattleState.GameOver("Terkena jebakan racun"))
 }
