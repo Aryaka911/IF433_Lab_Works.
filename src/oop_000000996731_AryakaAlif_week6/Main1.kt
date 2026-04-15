c49a59548a7d91bf94cfa09a5cc6a2f2df415960
Aryaka Alif <ardianun212@gmail.com>
2026-04-15 19:42:25 +0700

    week06: (task) instantiate smart devices in main

---
+++ src/oop_000000996731_AryakaAlif_week6/Main1.kt
@@ -0,0 +1,7 @@
+package oop_000000996731_AryakaAlif_week6
+
+fun main() {
+    val lamp = SmartLamp("L01", "Ruang Tamu")
+    val speaker = SmartSpeaker("S01", "Google Nest Dapur")
+    val cctv = SmartCCTV("C01", "Ezviz Garasi")
+}
