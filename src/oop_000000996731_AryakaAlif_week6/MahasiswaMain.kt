b8b363ee215e471a0fa94fae14f3a8a2b298e88b
Aryaka Alif <ardianun212@gmail.com>
2026-04-15 19:38:28 +0700

    week06: (task) create smart home interfaces

--- src/oop_000000996731_AryakaAlif_week6/MahasiswaMain.kt
+++ src/oop_000000996731_AryakaAlif_week6/MahasiswaMain.kt
@@ -2,9 +2,9 @@ package oop_000000996731_AryakaAlif_week6
 
 fun main() {
     val krs = KRS("Arya", "Derry")
+    val krs = KRS("Adam", "Kevin")
     krs.belajar()
     krs.mandi()
     krs.makan()
     krs.main_game()
     krs.jajan()
-}
