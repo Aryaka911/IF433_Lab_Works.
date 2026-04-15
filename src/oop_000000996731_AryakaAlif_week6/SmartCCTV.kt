f6deba849c19ceaa720b787a23563d133f5e2331
Aryaka Alif <ardianun212@gmail.com>
2026-04-15 19:40:48 +0700

    week06: (task) create SmartCCTV with compound interfaces

---
+++ src/oop_000000996731_AryakaAlif_week6/SmartCCTV.kt
@@ -0,0 +1,17 @@
+package oop_000000996731_AryakaAlif_week6
+
+class SmartCCTV (override val id: String, override val name: String) : SmartDevice, Switchable, Recordable {
+    override fun turnOn() {
+        println("CCTV $name dinyalakan.")
+        startRecord()
+    }
+
+    override fun turnOff() {
+        println("CCTV $name dimatikan.")
+        stopRecord()
+    }
+
+    override fun startRecord() {
+        println("CCTV $name mulai merekam.")
+    }
+}
