d5d4bb819fa2a95a54f66c7e50262f815aea69f4
Aryaka Alif <ardianun212@gmail.com>
2026-04-15 19:22:05 +0700

    week05: (task 2) add smart casting for EWallet topUp recovery

--- src/oop_000000996731_AryakaAlif_week5/PaymentMethod.kt
+++ src/oop_000000996731_AryakaAlif_week5/PaymentMethod.kt
@@ -1,4 +1,6 @@
 package oop_000000996731_AryakaAlif_week5
 
 class PaymentMethod {
-}
+    abstract class PaymentMethod(val accountName: String) {
+        abstract fun processPayment(amount: Double)
+    }
