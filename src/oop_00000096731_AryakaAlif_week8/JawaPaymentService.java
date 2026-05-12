c65dc9649c71d7532295d197623c4b66132f012d
Aryaka911 <ardianun212@gmail.com>
2026-05-12 13:40:26 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000096731_AryakaAlif_week8/JawaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000096731_AryakaAlif_week8
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
