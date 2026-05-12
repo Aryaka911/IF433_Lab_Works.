202893e568fa2645265ef7ef0e28a6ceed89ea32
Aryaka911 <ardianun212@gmail.com>
2026-05-12 14:05:52 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_00000096731_AryakaAlif_week9/TradingModels.kt
@@ -0,0 +1,9 @@
+package oop_00000096731_AryakaAlif_week9
+
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)
