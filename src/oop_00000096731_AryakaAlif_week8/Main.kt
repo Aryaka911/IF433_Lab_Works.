0646014bfecfb92751e2badace45530e60f8dcfd
Aryaka911 <ardianun212@gmail.com>
2026-05-12 13:27:55 +0700

    week08: create mixed data collection

--- src/oop_00000096731_AryakaAlif_week8/Main.kt
+++ src/oop_00000096731_AryakaAlif_week8/Main.kt
@@ -8,12 +8,21 @@ fun main() {
     println("Tujuan pengiriman: $destination")
 
     println("/n=== TEST LET BLOCK ===")
+    println("\n=== TEST LET BLOCK ===")
     val validOrder = Order(null, 250000)
 
     val receipt = validOrder.totalPrice?.let { price ->
-        val tax = price * 0.11
-        "Transaksi Valid. Harga: Rp$price, Pajak: Rp$tax"
+        @@ -16,4 +16,13 @@
     } ?: "Transaksi Invalid: Harga belum di-set!"
 
     println(receipt)
+
+    println("\n=== TEST SAFE CASTING ===")
+    val mixedData: List<Any> = listOf(
+        "Smartphone",
+        1500000,
+        UserProfile("Andi", null),
+        "Laptop",
+        4500000.0
+    )
 }
