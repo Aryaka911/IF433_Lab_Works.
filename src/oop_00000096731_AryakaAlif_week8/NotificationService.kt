490a693401a9baca6bed1f035a0a73129d80b1ab
Aryaka911 <ardianun212@gmail.com>
2026-05-12 13:23:17 +0700

    week08: implement smart cast in function

---
+++ src/oop_00000096731_AryakaAlif_week8/NotificationService.kt
@@ -0,0 +1,15 @@
+package oop_00000096731_AryakaAlif_week8
+
+class NotificationService {
+    fun sendEmail (emailAddress: String) {
+        println("Mengirim email ke: $emailAddress")
+    }
+
+    fun processUser (user: UserProfile) {
+        if (user.email != null) {
+            sendEmail(user.email)
+        } else {
+            println("User ${user.name} tidak memiliki email.")
+        }
+    }
+}
