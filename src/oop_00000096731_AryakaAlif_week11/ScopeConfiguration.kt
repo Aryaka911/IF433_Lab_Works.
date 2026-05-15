5b714bfd97d56d26f46d8d46d548ccf9e02667c2
Aryaka911 <ardianun212@gmail.com>
2026-05-15 15:37:38 +0700

    week11: implement also for logging side effects

--- src/oop_00000096731_AryakaAlif_week11/ScopeConfiguration.kt
+++ src/oop_00000096731_AryakaAlif_week11/ScopeConfiguration.kt
@@ -1,7 +1,6 @@
 package oop_00000096731_AryakaAlif_week11
 
 data class User(var name: String = "", var age: Int = 0)
-data class User(var name: String = "", var age: Int = 0)
 
 fun main() {
     println("=== TEST APPLY ===")
@@ -10,4 +9,11 @@ fun main() {
         age = 25
     }
     println(user)
+
+    println("\n=== TEST ALSO ===")
+    val numbers = mutableListOf(1, 2, 3)
+    numbers.also {
+        println("Log Sebelum ditambah: $it")
+    }.add(4)
+    println("Setelah ditambah: $numbers")
 }
