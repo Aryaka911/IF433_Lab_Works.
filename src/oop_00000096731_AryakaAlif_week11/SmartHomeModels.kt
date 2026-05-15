5efbdb5a148fb3b4d685d903c1f3329eb35f6122
Aryaka911 <ardianun212@gmail.com>
2026-05-15 15:38:53 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_00000096731_AryakaAlif_week11/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_00000096731_AryakaAlif_week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
